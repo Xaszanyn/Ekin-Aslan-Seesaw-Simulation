c3385947cfdd4ca30cd8d060cb42a360c7e19646
Ekin Aslan <ekinaslan.js@gmail.com>
2025-10-24 16:41:52 +0300

    Preview Object

---
+++ script.js
@@ -0,0 +1,55 @@
+const objectScreen = document.querySelector("#object-screen");
+const preview = document.querySelector("#preview");
+const nextWeightText = document.querySelector("#next-weight span");
+const seesaw = document.querySelector("#seesaw");
+
+var state = {
+  left: [],
+  right: [],
+  tiltAngle: 0,
+};
+
+var nextWeight = Math.ceil(Math.random() * 10);
+var previewWeight;
+var previewPosition;
+
+function setNextObject() {
+  preview.style.width = `${45 + (nextWeight - 1) * 3}px`;
+  preview.style.height = `${45 + (nextWeight - 1) * 3}px`;
+  preview.style.backgroundColor = `hsl(${Math.ceil(
+    Math.random() * 360
+  )} 100% 70%)`;
+  preview.textContent = `${nextWeight} kg`;
+  previewWeight = nextWeight;
+  nextWeight = Math.ceil(Math.random() * 10);
+  nextWeightText.textContent = `${nextWeight}.0 kg`;
+}
+
+function addObject(weight, distance, direction) {
+  state[direction ? "left" : "right"].push([weight, distance]);
+  localStorage.setItem("seesaw", JSON.stringify(state));
+}
+
+objectScreen.addEventListener("mousemove", ({ offsetX, offsetY }) => {
+  preview.style.display = "flex";
+  offsetX = Math.min(400, Math.max(0, offsetX));
+  previewPosition = [offsetX, offsetY];
+  preview.style.top = `${offsetY}px`;
+  preview.style.left = `${offsetX}px`;
+});
+
+objectScreen.addEventListener(
+  "mouseleave",
+  () => (preview.style.display = "none")
+);
+
+objectScreen.addEventListener("click", () => {
+  addObject(
+    previewWeight,
+    Math.abs(200 - previewPosition[0]),
+    previewPosition[0] <= 200
+  );
+  setNextObject();
+});
+
+setNextObject();
